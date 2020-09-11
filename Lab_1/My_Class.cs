3d69297d0fcaf4f041e641f8463e744bddb5901a
ArtyomKolosov2 <62713674+ArtyomKolosov2@users.noreply.github.com>
2020-09-11 10:56:42 +0300

    Добавьте файлы проекта.

---
+++ Lab_1/My_Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_1
+{
+    public static class My_Class
+    {
+        public static void ShowHello()
+        {
+            Console.WriteLine("Hello from my class!");
+        }
+    }
+}
